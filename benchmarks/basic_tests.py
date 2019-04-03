79a78540be208e557774b56f5abbca046ad1b89c
Jeremy White <jtwhite1000@gmail.com>
2019-04-03 15:45:15 +1300

    added benchmarks dir

---
+++ benchmarks/basic_tests.py
@@ -0,0 +1,264 @@
+import os
+import sys
+import shutil
+import platform
+import numpy as np
+import pandas as pd
+import platform
+import pyemu
+
+bin_path = os.path.join("test_bin")
+if "linux" in platform.platform().lower():
+    bin_path = os.path.join(bin_path,"linux")
+elif "darwin" in platform.platform().lower():
+    bin_path = os.path.join(bin_path,"mac")
+else:
+    bin_path = os.path.join(bin_path,"win")
+
+bin_path = os.path.abspath("test_bin")
+os.environ["PATH"] += os.pathsep + bin_path
+
+
+bin_path = os.path.join("..","..","..","bin")
+
+
+use_intel= os.getenv('USE_INTEL', False)
+# if len(sys.argv) > 1 and sys.argv[1].lower() == 'intel':
+#     use_intel = True
+#     print("using intel windows binaries")
+
+
+if "windows" in platform.platform().lower():
+    if use_intel:
+        print("using intel windows binaries")
+        exe_path = os.path.join(bin_path, "iwin", "ipestpp-ies.exe")
+    else:
+        exe_path = os.path.join(bin_path, "win", "pestpp-ies.exe")
+elif "darwin" in platform.platform().lower():
+    exe_path = os.path.join(bin_path,  "mac", "pestpp-ies")
+else:
+    exe_path = os.path.join(bin_path, "linux", "pestpp-ies")
+
+
+noptmax = 4
+num_reals = 20
+port = 4020
+
+def basic_test(model_d="ies_10par_xsec"):
+    pyemu.Ensemble.reseed()
+    base_d = os.path.join(model_d, "template")
+    new_d = os.path.join(model_d, "test_template")
+    if os.path.exists(new_d):
+        shutil.rmtree(new_d)
+    shutil.copytree(base_d, new_d)
+    print(platform.platform().lower())
+    local=True
+    if "linux" in platform.platform().lower() and "10par" in model_d:
+        #print("travis_prep")
+        #prep_for_travis(model_d)
+        local=False
+    pst = pyemu.Pst(os.path.join(new_d, "pest.pst"))
+    print(pst.model_command)
+    
+    # set first par as fixed
+    #pst.parameter_data.loc[pst.par_names[0], "partrans"] = "fixed"
+
+    pst.observation_data.loc[pst.nnz_obs_names,"weight"] = 1.0
+
+    # set noptmax
+    pst.control_data.noptmax = noptmax
+
+    # wipe all pestpp options
+    pst.pestpp_options = {}
+    pst.pestpp_options["ies_num_reals"] = num_reals
+    pst.pestpp_options["lambda_scale_fac"] = 1.0
+    pst.pestpp_options["ies_lambda_mults"] = 1.0
+    # write a generic 2D cov
+    if os.path.exists(os.path.join(new_d,"prior.jcb")):
+        cov = pyemu.Cov.from_binary(os.path.join(new_d,"prior.jcb"))
+        #cov.to_ascii(os.path.join(new_d,"prior.cov"))
+    elif os.path.exists(os.path.join(new_d, "prior.cov")):
+        cov = pyemu.Cov.from_ascii(os.path.join(new_d, "prior.cov"))
+    else:
+        cov = pyemu.Cov.from_parameter_data(pst)
+        cov = pyemu.Cov(cov.as_2d, names=cov.row_names)
+        #cov.to_ascii(os.path.join(new_d, "prior.cov"))
+        cov.to_binary(os.path.join(new_d, "prior.jcb"))
+
+    # draw some ensembles
+    idx = [i for i in range(num_reals)]
+    idx[-1] = "base"
+    pe = pyemu.ParameterEnsemble.from_gaussian_draw(pst, cov=cov, num_reals=num_reals,
+                                                    use_homegrown=True,group_chunks=True)
+    pe.index = idx
+    pe.to_csv(os.path.join(new_d, "par.csv"))
+    pe.to_binary(os.path.join(new_d, "par.jcb"))
+    pe.to_csv(os.path.join(new_d, "sweep_in.csv"))
+    pe.loc[:, pst.adj_par_names].to_csv(os.path.join(new_d, "par_some.csv"))
+    pe.iloc[:-3, :].to_csv(os.path.join(new_d, "restart_failed_par.csv"))
+    oe = pyemu.ObservationEnsemble.from_id_gaussian_draw(pst, num_reals=num_reals)
+    oe.index = idx
+    oe.to_csv(os.path.join(new_d, "obs.csv"))
+    oe.iloc[:-3, :].to_csv(os.path.join(new_d, "restart_failed_base_obs.csv"))
+    oe.to_binary(os.path.join(new_d, "obs.jcb"))
+
+    pst.write(os.path.join(new_d, "pest.pst"))
+
+    
+    m_d = os.path.join(model_d,"master_pestpp_sen")
+    if os.path.exists(m_d):
+        shutil.rmtree(m_d)
+    pyemu.os_utils.start_slaves(new_d, exe_path.replace("-ies","-sen"), "pest.pst", 5, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+    df = pd.read_csv(os.path.join(m_d, "pest.mio"),index_col=0)
+
+    # run sweep
+    m_d = os.path.join(model_d,"master_sweep1")
+    if os.path.exists(m_d):
+        shutil.rmtree(m_d)
+    pyemu.os_utils.start_slaves(new_d, exe_path.replace("-ies","-swp"), "pest.pst", 5, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+    df = pd.read_csv(os.path.join(m_d, "sweep_out.csv"),index_col=0)
+    
+    m_d = os.path.join(model_d,"master_pestpp-glm")
+    if os.path.exists(m_d):
+        shutil.rmtree(m_d)
+    pyemu.os_utils.start_slaves(new_d, exe_path.replace("-ies","-glm"), "pest.pst", 10, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+
+    m_d = os.path.join(model_d,"master_pestpp-ies")
+    if os.path.exists(m_d):
+        shutil.rmtree(m_d)
+    pyemu.os_utils.start_slaves(new_d, exe_path, "pest.pst", 10, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+
+
+
+def glm_save_binary_test():
+    model_d = "ies_10par_xsec"
+    local = True
+    if "linux" in platform.platform().lower() and "10par" in model_d:
+        # print("travis_prep")
+        # prep_for_travis(model_d)
+        local = False
+
+    t_d = os.path.join(model_d, "template")
+    m_d = os.path.join(model_d, "master_save_binary")
+    if os.path.exists(m_d):
+        shutil.rmtree(m_d)
+    pst = pyemu.Pst(os.path.join(t_d, "pest.pst"))
+    pst.pestpp_options = {"num_reals":30,"save_binary":True}
+    pst.write(os.path.join(t_d, "pest_save_binary.pst"))
+    pyemu.os_utils.start_slaves(t_d, exe_path.replace("-ies", "-glm"), "pest_save_binary.pst", 10, master_dir=m_d,
+                                slave_root=model_d, local=local, port=port)
+
+    pe = pyemu.ParameterEnsemble.from_binary(pst=pst,filename=os.path.join(m_d,"pest_save_binary.post.paren.jcb"))
+    pe = pyemu.ObservationEnsemble.from_binary(pst=pst,filename=os.path.join(m_d, "pest_save_binary.post.obsen.jcb"))
+
+def sweep_forgive_test():
+    model_d = "ies_10par_xsec"
+    local=True
+    if "linux" in platform.platform().lower() and "10par" in model_d:
+        #print("travis_prep")
+        #prep_for_travis(model_d)
+        local=False
+    
+    t_d = os.path.join(model_d,"template")
+    m_d = os.path.join(model_d,"master_sweep_forgive")
+    if os.path.exists(m_d):
+        shutil.rmtree(m_d)
+    pst = pyemu.Pst(os.path.join(t_d,"pest.pst"))
+    pe = pyemu.ParameterEnsemble.from_uniform_draw(pst,num_reals=50)#.loc[:,pst.par_names[:2]]
+    pe.loc[:,pst.par_names[2:]] = pst.parameter_data.loc[pst.par_names[2:],"parval1"].values
+    pe.to_csv(os.path.join(t_d,"sweep_in.csv"))
+    pst.pestpp_options["sweep_forgive"] = True
+    pst.write(os.path.join(t_d,"pest_forgive.pst"))
+    pyemu.os_utils.start_slaves(t_d, exe_path.replace("-ies","-swp"), "pest_forgive.pst", 10, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+    df1 = pd.read_csv(os.path.join(m_d, "sweep_out.csv"),index_col=0)
+
+    pe = pe.loc[:,pst.par_names[:2]]
+    pe.to_csv(os.path.join(t_d,"sweep_in.csv"))
+    pst.pestpp_options["sweep_forgive"] = True
+    pst.write(os.path.join(t_d,"pest_forgive.pst"))
+    pyemu.os_utils.start_slaves(t_d, exe_path.replace("-ies","-swp"), "pest_forgive.pst", 10, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+    df2 = pd.read_csv(os.path.join(m_d, "sweep_out.csv"),index_col=0)
+    diff = df1 - df2
+    print(diff.max())
+    assert diff.max().max() == 0.0
+
+
+def inv_regul_test():
+    model_d = "ies_10par_xsec"
+    local=True
+    if "linux" in platform.platform().lower() and "10par" in model_d:
+        #print("travis_prep")
+        #prep_for_travis(model_d)
+        local=False
+    
+    t_d = os.path.join(model_d,"template")
+    m_d = os.path.join(model_d,"master_inv_regul")
+    if os.path.exists(m_d):
+        shutil.rmtree(m_d)
+    pst = pyemu.Pst(os.path.join(t_d,"pest.pst"))
+    #pyemu.helpers.zero_order_tikhonov(pst)
+    #pst.control_data.pestmode = "regularization"
+    pst.reg_data.phimlim = 2
+    pst.reg_data.phimaccept = 2.2
+    pst.control_data.noptmax = 10
+    pst.write(os.path.join(t_d,"pest_regul.pst"))
+    pyemu.os_utils.start_slaves(t_d, exe_path.replace("-ies","-glm"), "pest_regul.pst", 10, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+    
+
+def tie_by_group_test():
+    model_d = "ies_10par_xsec"
+    local=True
+    if "linux" in platform.platform().lower() and "10par" in model_d:
+        #print("travis_prep")
+        #prep_for_travis(model_d)
+        local=False
+    
+    t_d = os.path.join(model_d,"template")
+    m_d = os.path.join(model_d,"master_tie_by_group")
+    if os.path.exists(m_d):
+        shutil.rmtree(m_d)
+    pst = pyemu.Pst(os.path.join(t_d,"pest.pst")) 
+    par = pst.parameter_data
+    tied_names = pst.adj_par_names[:3]
+    par.loc[tied_names[1:3],"partrans"] = "tied"
+    par.loc[tied_names[1:3],"partied"] = tied_names[0]
+    pst.pestpp_options = {}
+    pst.pestpp_options["ies_num_reals"] = 10
+    pst.pestpp_options["ies_lambda_mults"] = 1.0
+    pst.pestpp_options["lambda_scale_fac"] = 1.0
+    pst.pestpp_options["tie_by_group"] = True
+    pst.control_data.noptmax = 2
+    pst.write(os.path.join(t_d,"pest_tied.pst"))
+    
+    pyemu.os_utils.start_slaves(t_d, exe_path, "pest_tied.pst", 10, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+    df = pd.read_csv(os.path.join(m_d,"pest_tied.{0}.par.csv".format(pst.control_data.noptmax)),index_col=0)
+    df.columns = df.columns.str.lower()
+    print(df.loc[:,tied_names].std(axis=1).apply(np.abs).max())
+    assert df.loc[:,tied_names].std(axis=1).apply(np.abs).max() < 1.0e-8
+
+    df.to_csv(os.path.join(t_d,"sweep_in.csv"))
+    pyemu.os_utils.start_slaves(t_d, exe_path.replace("-ies","-swp"), "pest_tied.pst", 5, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+
+    pyemu.os_utils.start_slaves(t_d, exe_path.replace("-ies","-glm"), "pest_tied.pst", 5, master_dir=m_d,
+                           slave_root=model_d,local=local,port=port)
+    jco = pyemu.Jco.from_binary(os.path.join(m_d,"pest_tied.jcb"))
+    assert jco.shape[1] == 2,jco.shape
+
+
+
+
+if __name__ == "__main__":
+    #basic_test("ies_10par_xsec")
+    glm_save_binary_test()
+    #sweep_forgive_test()
+    #inv_regul_test()
+    #tie_by_group_test()
