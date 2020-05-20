554c64cd9f306bbb274c9708537d43c6af09f2b6
Mike Taves <mwtoews@gmail.com>
2020-05-20 22:09:19 +1200

    Constraints::initialize - missing ".clear()" for ctl_ord_pi_constraint_names vector

--- src/libs/pestpp_common/constraints.cpp
+++ src/libs/pestpp_common/constraints.cpp
@@ -48,7 +48,7 @@ void Constraints::initialize(vector<string>& ctl_ord_dec_var_names, Parameters* 
 	dbl_max = _dbl_max;
 	rand_gen = std::mt19937(pest_scenario.get_pestpp_options().get_random_seed());
 	ctl_ord_obs_constraint_names.clear();
-	ctl_ord_pi_constraint_names;
+	ctl_ord_pi_constraint_names.clear();
 	constraint_sense_map.clear();
 	constraint_sense_name.clear();
 	nz_obs_names.clear();
