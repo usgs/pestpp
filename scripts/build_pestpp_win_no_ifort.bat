9e6e5c007af9cfccd53d749811a092aa07849614
jwhite-usgs <jwhiteusgs@gmail.com>
2020-06-26 11:30:08 -0600

    added more comments to constraints class, working on getting ifort and pestpp-pso going

--- scripts/build_pestpp_win_no_ifort.bat
+++ scripts/build_pestpp_win_no_ifort.bat
@@ -9,7 +9,7 @@ rmdir /Q /S build
 mkdir build
 call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\compilervars.bat" intel64
 cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=icl ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=icl -DCMAKE_Fortran_COMPILER=ifort ..
 ninja
 cpack -G ZIP
 copy /y *.zip ..\
