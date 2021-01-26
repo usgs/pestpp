39129e863a52b335354fa3a94c0b15343925faff
jtwhite79 <jtwhite1000@gmail.com>
2021-01-26 15:22:42 -0700

    added logic to catch unsuccessful partial update

--- src/libs/pestpp_common/EnsembleSmoother.cpp
+++ src/libs/pestpp_common/EnsembleSmoother.cpp
@@ -2907,25 +2907,29 @@ bool IterEnsembleSmoother::solve_new()
 			update_reals_by_phi(pe_lams[best_idx], oe_lams[best_idx]);
 			ph.update(oe, pe);
 			//re-check phi
-			best_mean = ph.get_mean(L2PhiHandler::phiType::COMPOSITE);
-			best_std = ph.get_std(L2PhiHandler::phiType::COMPOSITE);
-			//replace the last entry in the best mean phi tracker
-			best_mean_phis[best_mean_phis.size() - 1] = best_mean;
-			message(1, "current best mean phi (after updating reduced-phi reals): ", best_mean);
-			if (best_mean < last_best_mean * acc_fac)
+			double new_best_mean = ph.get_mean(L2PhiHandler::phiType::COMPOSITE);
+			if (new_best_mean < best_mean)
 			{
-				if (best_std < last_best_std * acc_fac)
+				best_mean = new_best_mean;
+				best_std = ph.get_std(L2PhiHandler::phiType::COMPOSITE);
+				//replace the last entry in the best mean phi tracker
+				best_mean_phis[best_mean_phis.size() - 1] = best_mean;
+				message(1, "current best mean phi (after updating reduced-phi reals): ", best_mean);
+				if (best_mean < last_best_mean * acc_fac)
 				{
-					double new_lam = lam_vals[best_idx] * lam_dec;
-					new_lam = (new_lam < lambda_min) ? lambda_min : new_lam;
-					message(0, "updating lambda to ", new_lam);
-					last_best_lam = new_lam;
+					if (best_std < last_best_std * acc_fac)
+					{
+						double new_lam = lam_vals[best_idx] * lam_dec;
+						new_lam = (new_lam < lambda_min) ? lambda_min : new_lam;
+						message(0, "updating lambda to ", new_lam);
+						last_best_lam = new_lam;
+					}
+					else
+					{
+						message(0, "not updating lambda (standard deviation reduction criteria not met)");
+					}
+					last_best_std = best_std;
 				}
-				else
-				{
-					message(0, "not updating lambda (standard deviation reduction criteria not met)");
-				}
-				last_best_std = best_std;
 			}
 			message(1, "returing to lambda calculations...");
 			return false;
@@ -3080,32 +3084,37 @@ bool IterEnsembleSmoother::solve_new()
 		update_reals_by_phi(pe_lams[best_idx], oe_lam_best);
 		ph.update(oe, pe);
 		//re-check phi
-		best_mean = ph.get_mean(L2PhiHandler::phiType::COMPOSITE);
-		best_std = ph.get_std(L2PhiHandler::phiType::COMPOSITE);
-		//replace the last entry in the best mean phi tracker
-		best_mean_phis[best_mean_phis.size() - 1] = best_mean;
-		message(1, "current best mean phi (after updating reduced-phi reals): ", best_mean);
-		if (best_mean < last_best_mean * acc_fac)
+		double new_best_mean = ph.get_mean(L2PhiHandler::phiType::COMPOSITE);
+		if (new_best_mean < best_mean)
 		{
-			if (best_std < last_best_std * acc_fac)
+			best_mean = new_best_mean;
+
+			best_std = ph.get_std(L2PhiHandler::phiType::COMPOSITE);
+			//replace the last entry in the best mean phi tracker
+			best_mean_phis[best_mean_phis.size() - 1] = best_mean;
+			message(1, "current best mean phi (after updating reduced-phi reals): ", best_mean);
+			if (best_mean < last_best_mean * acc_fac)
 			{
-				double new_lam = lam_vals[best_idx] * lam_dec;
-				new_lam = (new_lam < lambda_min) ? lambda_min : new_lam;
-				message(0, "updating lambda to ", new_lam);
-				last_best_lam = new_lam;
+				if (best_std < last_best_std * acc_fac)
+				{
+					double new_lam = lam_vals[best_idx] * lam_dec;
+					new_lam = (new_lam < lambda_min) ? lambda_min : new_lam;
+					message(0, "updating lambda to ", new_lam);
+					last_best_lam = new_lam;
+				}
+				else
+				{
+					message(0, "not updating lambda (standard deviation reduction criteria not met)");
+				}
+				last_best_std = best_std;
 			}
 			else
 			{
-				message(0, "not updating lambda (standard deviation reduction criteria not met)");
+				double new_lam = last_best_lam * lam_inc;
+				new_lam = (new_lam > lambda_max) ? lambda_max : new_lam;
+				message(0, "incresing lambda to: ", new_lam);
+				last_best_lam = new_lam;
 			}
-			last_best_std = best_std;
-		}
-		else
-		{
-			double new_lam = last_best_lam * lam_inc;
-			new_lam = (new_lam > lambda_max) ? lambda_max : new_lam;
-			message(0, "incresing lambda to: ", new_lam);
-			last_best_lam = new_lam;
 		}
 	}
 	//report_and_save();
