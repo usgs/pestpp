878c37be0088b2ba510a4eb5a9b599bdb4654ead
jdub <JWhite@localadmins-MacBook-Pro.local>
2022-08-22 11:49:22 -0500

    fix in Cov.from_csv() - need to explicitly clear row and col names in case if re-read

--- src/libs/pestpp_common/covariance.cpp
+++ src/libs/pestpp_common/covariance.cpp
@@ -422,6 +422,9 @@ void Mat::from_csv(const string &filename)
 		throw runtime_error("error reading header (first) line from csv file :");
 	pest_utils::strip_ip(line);
 	pest_utils::upper_ip(line);
+	col_names.clear();
+	row_names.clear();
+
 	pest_utils::tokenize(line, col_names, ",", false);
 	col_names.erase(col_names.begin()); //drop the index label
 	vector<Eigen::Triplet<double>> triplet_list;
@@ -479,6 +482,7 @@ void Mat::from_csv(const string &filename)
 	irow++;
 	}
 	matrix.resize(row_names.size(), col_names.size());
+	matrix.setZero();
 	matrix.setFromTriplets(triplet_list.begin(), triplet_list.end());
 
 }
