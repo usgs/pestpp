04c0df9b8a572a20f8e4c113ef072e5321ab31c1
jdub <jtwhite1000@gmail.com>
2023-08-06 17:55:05 +0800

    switched to pso as default

--- src/libs/pestpp_common/pest_data_structs.cpp
+++ src/libs/pestpp_common/pest_data_structs.cpp
@@ -1874,7 +1874,7 @@ void PestppOptions::set_defaults()
 	set_sqp_update_hessian(false);
 	set_sqp_scale_facs(vector<double>{0.00001, 0.0001,0.0005, 0.001, 0.0025, 0.005, 0.01, 0.05, 0.075, 0.1, 0.25,0.5, 1.0,2.,5.,10.,});
 
-	set_mou_generator("DE");
+	set_mou_generator("PSO");
 	set_mou_population_size(100);
 	set_mou_dv_population_file("");
 	set_mou_obs_population_restart_file("");
