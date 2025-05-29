da6c8d607df93f454333f57746269f1192651b22
jwhite <jtwhite1000@gmail.com>
2025-05-29 12:13:54 -0600

    removed samoo test folders

--- benchmarks/seawat/pest/artrch.dat.tpl
+++
@@ -1,3 +0,0 @@
-ptf ~
-ar_rate_1   ~   ar_rate_1     ~
-ar_rate_2   ~   ar_rate_2     ~
--- benchmarks/zdt_fitness_expt/pest/MODELRUN_MOGP.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-"C:\Users\mac732\AppData\Local\Programs\R\R-4.3.1\bin\Rscript.exe" ./model/MOGP_1.R > NUL
-
-:end	
