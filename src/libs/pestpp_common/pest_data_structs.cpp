8e8361a846697532f17089149d22739369f2581f
jwhite <jtwhite1000@gmail.com>
2025-07-02 08:08:18 -0600

    set default panther timeout to 500ms

--- src/libs/pestpp_common/pest_data_structs.cpp
+++ src/libs/pestpp_common/pest_data_structs.cpp
@@ -2130,7 +2130,7 @@ void PestppOptions::set_defaults()
 	set_panther_echo(true);
     set_panther_transfer_on_finish(vector<string>{});
     set_panther_transfer_on_fail(vector<string>{});
-    set_panther_timeout_milliseconds(-999);
+    set_panther_timeout_milliseconds(500);
     set_panther_echo_interval_milliseconds(1000);
 
 }
