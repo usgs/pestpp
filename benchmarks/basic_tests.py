0425a7b99972f8bfde13ad79cee727b83487b944
jwhite <jtwhite1000@gmail.com>
2025-08-18 15:03:01 -0600

    added more get-modflow()

--- benchmarks/basic_tests.py
+++ benchmarks/basic_tests.py
@@ -52,11 +52,11 @@ def basic_test(model_d="ies_10par_xsec"):
     shutil.copytree(base_d, new_d)
     print(platform.platform().lower())
     pst = pyemu.Pst(os.path.join(new_d, "pest.pst"))
-    # cmd = pst.model_command[0].split()
-    # print(cmd)
-    # cmd = "\"\"{0}\" \"{1}\"\"".format(cmd[0],cmd[1])
-    # print(cmd)
-    # pst.model_command.append(cmd)
+    cmd = pst.model_command[0].split()
+    print(cmd)
+    cmd = "\"\"{0}\" \"{1}\"\"".format(cmd[0],cmd[1])
+    print(cmd)
+    pst.model_command.append(cmd)
     
     # set first par as fixed
     #pst.parameter_data.loc[pst.par_names[0], "partrans"] = "fixed"
