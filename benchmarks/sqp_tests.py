fa5364dd30570b44ab522cb9655c608552457543
rqmacasieb <reygie.macasieb@research.uwa.edu.au>
2026-01-21 14:10:36 +0800

    fix sqp basic test

--- benchmarks/sqp_tests.py
+++ benchmarks/sqp_tests.py
@@ -48,6 +48,7 @@ def basic_sqp_test():
     pst = pyemu.Pst(os.path.join(t_d,"freyberg6_run_opt.pst"))
     pst.pestpp_options.pop("ies_par_en")
     pst.control_data.noptmax = 0
+    pst.model_command = ["../../test_bin/win/mf6.exe"]
     pst.write(os.path.join(t_d,"freyberg6_run_sqp.pst"))
     pyemu.os_utils.run("{0} freyberg6_run_sqp.pst".format(exe_path),cwd=t_d)
 
@@ -643,9 +644,9 @@ if __name__ == "__main__":
     #if not os.path.exists(os.path.join("..","bin")):
     #    os.mkdir(os.path.join("..","bin"))
     #shutil.copy2(os.path.join("..","exe","windows","x64","Debug","pestpp-sqp.exe"),os.path.join("..","bin","pestpp-sqp.exe"))
-    #basic_sqp_test()
-    #rosenbrock_single_linear_constraint(nit=1)
-    #dewater_basic_test()
+    basic_sqp_test()
+    # rosenbrock_single_linear_constraint(nit=1)
+    # dewater_basic_test()
     dewater_slp_opt_test()
     #rosenc_test()
     #m_d = rosenc_test()
