0db6929cc93a63c196fb64801b8e68047f6237a8
jwhite <jwhite@m5max.local>
2026-05-14 07:58:55 -0600

    resaving prior par en if runs failed during the prior eval

--- src/libs/pestpp_common/EnsembleMethodUtils.cpp
+++ src/libs/pestpp_common/EnsembleMethodUtils.cpp
@@ -5771,8 +5771,35 @@ void EnsembleMethod::initialize(int cycle, bool run, bool use_existing)
                     ss << endl;
             }
             message(0,ss.str());
+        	ss.str("");
+        	if (pest_scenario.get_pestpp_options().get_save_dense())
+        	{
 
+        		ss << file_manager.get_base_filename();
+        		if (cycle != NetPackage::NULL_DA_CYCLE)
+        			ss << "." << cycle;
+        		ss << ".0.par" << dense_file_ext;
+        		pe.to_dense_unordered(ss.str());
+        	}
+        	else if (pest_scenario.get_pestpp_options().get_save_binary())
+        	{
+        		ss << file_manager.get_base_filename();
+        		if (cycle != NetPackage::NULL_DA_CYCLE)
+        			ss << "." << cycle;
+        		ss << ".0.par.jcb";
+        		pe.to_binary(ss.str());
+        	}
+        	else
+        	{
+        		ss << file_manager.get_base_filename();
+        		if (cycle != NetPackage::NULL_DA_CYCLE)
+        			ss << "." << cycle;
+        		ss << ".0.par.csv";
+        		pe.to_csv(ss.str());
+        	}
+        	message(1, "saved initial truncated parameter ensemble to ", ss.str());
         }
+
 
 		pe.transform_ip(ParameterEnsemble::transStatus::NUM);
 	}
